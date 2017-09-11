53b4a5715f5979c337d4e03d67f9007702a21902
RTG <ryan@sitesonix.net>
2017-09-11 18:27:06 -0500

    Correct org-babel-tangle git version of rtg-init.el

--- rtg-init.el
+++ rtg-init.el
@@ -7,7 +7,7 @@
 ;; Author: Ryan Griffith <ryan@sitesonix.net>
 ;; URL: https://sitesonix.net/
 ;; Version: 1.2
-;; Keywords: dvorak web gtd
+;; Keywords: org gtd web
 
 ;; This file is not a part of GNU Emacs.
 
@@ -30,21 +30,17 @@
 
 ;;; Sections:
 
-;; 0. Prelude Require Packages
-;; 1. Basic Editor Preferences
-;; 2. Terminals and Shells
-;; 3. Theme Preferences
-;; 4. Dired and Ibuffer Tweaks
-;; 5. Mode Activation/Keybindings
-;; 6. Org Mode Configuration
-;; 7. Web Development
-;; 8. Miscellaneous Stuff
-
-;;; Code:
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; 0. Prelude Require Packages @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 01. Introduction
+;; 02. Prelude Require Packages
+;; 03. Basic Editor Preferences
+;; 04. Terminals and Shells
+;; 05. Theme Preferences
+;; 06. Dired and Ibuffer Tweaks
+;; 07. Mode Activation/Keybindings
+;; 08. Org Mode Configuration
+;; 09. Web Development
+;; 10. Miscellaneous Stuff
+;; 11. Close Init
 
 (prelude-require-packages '(ac-emmet
                             ac-js2
@@ -78,41 +74,29 @@
                             web-beautify
                             ztree))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; 1. Basic Editor Preferences @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; Set user details
 (setq full-name "Ryan Griffith")
 (setq user-mail-address "ryan@sitesonix.net")
 
-;; Customize the scratch buffer (for me)
 (setq initial-scratch-message
       ";; The GNU GPL was not designed to be open source. - RMS\n\n")
 
-;; Set the default coding system to UTF-8
 (prefer-coding-system 'utf-8)
 (set-default-coding-systems 'utf-8)
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
 
-;; Set the font size
 (set-face-attribute 'default nil :height 100)
 
-;; Enable cursor blinking
 (blink-cursor-mode t)
 
-;; Use modeline flash as a visible bell
 (defun rtg/flash-mode-line ()
   (invert-face 'mode-line)
   (run-with-timer 0.05 nil 'invert-face 'mode-line))
 (setq-default
  ring-bell-function 'rtg/flash-mode-line)
 
-;; The scroll bar isn't necessary
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
-;; Display date and time in the mode line
 (setq display-time-day-and-date t
       display-time-12hr-format t)
 (display-time)
@@ -127,44 +111,30 @@
 (global-set-key (kbd "C-z <right>") 'windmove-right)
 ;; where S <left/right/up/down> still works outside org
 
-;; Smart tab behavior - indent or complete
 (setq tab-always-indent 'complete)
 
-;; minibuffer evaluations for paredit
 (add-hook 'eval-expression-minibuffer-setup-hook #'paredit-mode)
 
-;; Set frame title to show filename plus working directory
 (setq uniquify-buffer-name-style 'reverse)
 (setq inhibit-default-init t)
 (setq-default frame-title-format "%b (%f)")
 (global-set-key "\M-n" "\C-u1\C-v")
 (global-set-key "\M-p" "\C-u1\M-v")
 
-;; Mods and tweaks in the echo area
 (setq echo-keystrokes 0.1
       use-dialog-box nil
       visible-bell nil)
 
-;; Move default tilde ~ backup files to a backups directory
 (setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
 
-;; Cleanup whitespace before save
 (add-hook 'before-save-hook 'whitespace-cleanup)
 
-;; yes is always y
 (fset 'yes-or-no-p 'y-or-n-p)
 
-;; Add a new line at the end of a file
 (setq require-final-newline t)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; 2. Terminals and Shells @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; Setup bash for use with multi-term
 (setq multi-term-program "/bin/bash")
 
-;; Setup multi-term for practical use
 (when (require 'multi-term nil t)
   (global-set-key (kbd "C-x t") 'multi-term)
   (global-set-key (kbd "<C-next>") 'multi-term-next)
@@ -172,7 +142,6 @@
   (setq multi-term-buffer-name "mterm"
         multi-term-program "/bin/bash"))
 
-;; Multi-term keybindings
 (when (require 'term nil t) ; only if term can be loaded..
   (setq term-bind-key-alist
         (list (cons "C-c C-c" 'term-interrupt-subjob)
@@ -191,22 +160,14 @@
               (cons "M-y" 'term-send-raw-meta)
               (cons "C-y" 'term-send-raw))))
 
-;; Prompt for password and then open files owned by root
 (defadvice ido-find-file (after find-file-sudo activate)
   "Find file as root if necessary."
   (unless (and buffer-file-name
                (file-writable-p buffer-file-name))
     (find-alternate-file (concat "/sudo:root@gnutop:" buffer-file-name))))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; 3. Theme Preferences @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; Because I use Emacs built-in customize for themes
-;; (usually the sanityinc tomorrow night theme but not always...)
 (disable-theme 'zenburn)
 
-;; Enable transparency for frames and windows
 (set-frame-parameter (selected-frame) 'alpha '(92 50))
 (add-to-list 'default-frame-alist '(alpha 92 50))
 (eval-when-compile (require 'cl))
@@ -224,18 +185,11 @@
 ;; Toggle the transparency with F5
 (global-set-key [f5] 'toggle-transparency)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; 4. Dired and Ibuffer Tweaks @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; dired: human readable sizes and sort by size
 (setq dired-listing-switches "-alh")
 
-;; Work better with files in different directories
 (require 'find-dired)
 (setq find-ls-option '("-print0 | xargs -0 ls -ld" . "-ld"))
 
-;; Ibuffer: Use Gnus-style grouping for list
 (setq ibuffer-saved-filter-groups
       (quote (("default"
                ("dired" (mode . dired-mode))
@@ -268,7 +222,6 @@
           (lambda ()
             (ibuffer-switch-to-saved-filter-groups "default")))
 
-;; Ibuffer: use human readable Size column instead of original one
 (define-ibuffer-column size-h
   (:name "Size" :inline t)
   (cond
@@ -277,7 +230,6 @@
    ((> (buffer-size) 1000) (format "%7.1fk" (/ (buffer-size) 1000.0)))
    (t (format "%8d" (buffer-size)))))
 
-;; Modify the default ibuffer-formats
 (setq ibuffer-formats
       '((mark modified read-only " "
               (name 18 18 :left :elide)
@@ -288,48 +240,40 @@
               " "
               filename-and-process)))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 5. Mode Specific (general) @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; Enable ivy mode completion everywhere
 (ivy-mode 1)
 
-;; Basic ivy settings
 (setq ivy-use-virtual-buffers t)
 (setq ivy-count-format "(%d/%d) ")
 
-;; Disable guru-mode because arrow keys are sometimes useful
 (setq prelude-guru nil)
 
-;; Enable winner-mode
 (winner-mode 1)
 
-;; Enable which-key mode
 (which-key-mode)
 
-;; Enable undo-tree-mode visualization with C-x u
 (global-undo-tree-mode)
 
-;; Enable toggle for project explorer
 (global-set-key (kbd "C-c SPC") 'project-explorer-toggle)
 ;; NOTE: when outside of a project I like to use the built-in M-x speedbar
 
-;; Enable and set ztree keybindings
 (global-set-key (kbd "C-c z") 'ztree-diff)
 (global-set-key (kbd "C-c Z") 'ztree-dir)
 
-;; Enable and set multiple cursors keybindings
 (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 6. Org-mode Setup @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq org-agenda-files
+      (list "~/org/gtd.org"
+            "~/org/work.org"
+            "~/org/personal.org"))
 
-;; First to ensure that auto fill mode is an option for org other text docs
+(defun gtd ()
+  (interactive)
+  (find-file "~/org/gtd.org")
+  )
+
 (add-hook 'text-mode-hook
           (lambda ()
             (when (y-or-n-p "Auto Fill mode? ")
@@ -338,7 +282,6 @@
 (global-set-key (kbd "C-c q") 'auto-fill-mode)
 ;; otherwise set visual-line-mode or olivetti for distraction-free writing
 
-;; Setup org mode agenda
 (add-to-list 'load-path "~/emacs/org")
 (require 'org)
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
@@ -346,29 +289,12 @@
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done t)
 
-;; Main files are found here. Add new project files to the list as needed
-(setq org-agenda-files
-      (list "~/org/gtd.org"
-            "~/org/work.org"
-            "~/org/personal.org"))
-
-;; Interactive gtd file
-(defun gtd ()
-  (interactive)
-  (find-file "~/org/gtd.org")
-  )
-
-;; Set return to activate a link
-(setq org-return-follows-link t)
-
-;; Custom org-agenda commands
 (setq org-agenda-custom-commands
       '(("w" todo "WAITING" nil)
         ("n" todo "NEXT" nil)
         ("d" "Agenda + Next Actions" ((agenda) (todo "NEXT"))))
       )
 
-;; function to capture a todo
 (defun rtg/org-capture-todo ()
   (interactive)
   "Capture a TODO item"
@@ -376,36 +302,29 @@
 ;; bind
 (define-key global-map (kbd "C-7") 'rtg/org-capture-todo)
 
-;; Org-projectile for per-repo TODO files
-(require 'org-projectile)
-(org-projectile:per-repo)
-(setq org-projectile:per-repo-filename "project.org")
-(setq org-agenda-files (append org-agenda-files (org-projectile:todo-files)))
-(global-set-key (kbd "C-c c") 'org-capture)
-(global-set-key (kbd "C-c n p") 'org-projectile:project-todo-completing-read)
+;; Org-projectile for per-repo TODO files -- package is broken
+;; (require 'org-projectile)
+;; (org-projectile:per-repo)
+;; (setq org-projectile:per-repo-filename "project.org")
+;; (setq org-agenda-files (append org-agenda-files (org-projectile:todo-files)))
+;; (global-set-key (kbd "C-c c") 'org-capture)
+;; (global-set-key (kbd "C-c n p") 'org-projectile:project-todo-completing-read)
 
-;; Org-capture to personal.org file
 (setq org-default-notes-file "~/org/personal.org")
 ;; Org-capture keybinding
 (global-set-key (kbd "C-c c") 'org-capture)
 
-;; Refile: show all headings from all agenda files
 (setq org-refile-targets '((org-agenda-files . (:maxlevel . 5))))
 
-;; Extra org modules and export backends
+(setq org-return-follows-link t)
+
 (setq org-modules '(org-bbdb
                     org-gnus))
 (eval-after-load 'org
   '(org-load-modules-maybe t))
 
-;; Prepare stuff for org-export-backends
 (setq org-export-backends '(org latex html ascii))
 
-;; The following org-agenda hacks are borrowed from Sacha Chua's config
-;; http://pages.sachachua.com/.emacs.d/Sacha.html#org6eefca2
-;; What I trying to do was already out there. :-)
-
-;; Mark TODO as done by simply hitting 'x'
 (defun rtg/org-agenda-done (&optional arg)
   "Mark current TODO as done.
 This changes the line at point, all other lines in the agenda referring to
@@ -415,7 +334,6 @@ the same tree node, and the headline of the tree node in the Org-mode file."
 ;; Override the key definition for org-exit
 (define-key org-agenda-mode-map "x" 'rtg/org-agenda-done)
 
-;; Mark TODO as done with 'X' and then create new task at same level
 (defun rtg/org-agenda-mark-done-and-add-followup ()
   "Mark the current TODO as done and add another task after it.
 Creates it at the same level as the previous task, so it's better to use
@@ -427,7 +345,6 @@ this with to-do items than with projects or headings."
 ;; Override the key definition
 (define-key org-agenda-mode-map "X" 'rtg/org-agenda-mark-done-and-add-followup)
 
-;; Capture something based on the agenda
 (defun rtg/org-agenda-new ()
   "Create a new note or task at the current agenda item.
 Creates it at the same level as the previous task, so it's better to use
@@ -438,7 +355,6 @@ this with to-do items than with projects or headings."
 ;; New key assignment
 (define-key org-agenda-mode-map "N" 'rtg/org-agenda-new)
 
-;; Keep track of unscheduled tasks and stuck projects
 (defun rtg/org-agenda-list-unscheduled (&rest ignore)
   "Create agenda view for tasks that are unscheduled and not done."
   (let* ((org-agenda-todo-ignore-with-date t)
@@ -450,45 +366,31 @@ this with to-do items than with projects or headings."
         nil
         "\\<IGNORE\\>"))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 7. Web Development  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; Align code in a pretty way
 (global-set-key (kbd "C-x \\") #'align-regexp)
 
-;; Enable emmet for web mode and css mode
 (add-hook 'web-mode-hook 'emmet-mode)
 (add-hook 'css-mode-hook  'emmet-mode)
 
-;; Get company-mode to work with web-mode
 (eval-after-load 'company-etags
   '(progn
      (add-to-list 'company-etags-modes 'web-mode)))
 
 (eval-after-load 'web-mode '(define-key web-mode-map (kbd "s-d") 'php-jump))
 
-;; Set default mode for Javascript files
 (add-to-list 'auto-mode-alist '("\\.js" . js-mode))
 
-;; Set default mode for JSON files
 (add-to-list 'auto-mode-alist '("\\.json$" . json-mode))
 
-;; Add hooks for JS2 mode and auto-complete
 (add-hook 'js-mode-hook 'js2-minor-mode)
 (add-hook 'js2-mode-hook 'ac-js2-mode)
 
-;; yasnippet should be loaded before auto complete so that they can work together
-;; Load yasnippets
 (add-to-list 'load-path
              "~/.emacs.d/plugins/yasnippet")
 (require 'yasnippet)
 (yas-global-mode 1)
 
-;; yasnippet expansion and completion with dropdown
 (setq yas-prompt-functions '(yas-x-prompt yas-dropdown-prompt))
 
-;; Auto-complete
 (require 'auto-complete)
 (require 'auto-complete-config)
 (ac-config-default)
@@ -503,13 +405,9 @@ this with to-do items than with projects or headings."
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
 (ac-config-default)
 
-;;; set the trigger key so that it can work together with yasnippet on tab key,
-;;; if the word exists in yasnippet, pressing tab will cause yasnippet to
-;;; activate, otherwise, auto-complete will
 (ac-set-trigger-key "TAB")
 (ac-set-trigger-key "<tab>")
 
-;; Require web-mode for mixed templates with HTML, CSS and PHP
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
@@ -518,10 +416,8 @@ this with to-do items than with projects or headings."
 (add-to-list 'auto-mode-alist
             '("/\\(views\\|html\\|theme\\|templates\\)/.*\\.php\\'" . web-mode))
 
-;; make web-mode play nice with smartparens
 (setq web-mode-enable-auto-pairing nil)
 
-;; Require auto-complete for web-mode
 (require 'auto-complete)
 (require 'auto-complete-config)
 (ac-config-default)
@@ -531,22 +427,13 @@ this with to-do items than with projects or headings."
 (setq ac-auto-start nil)
 (global-set-key (kbd "C-<tab>") 'auto-complete)
 
-;; Require impatient mode to start after invoking simple httpd mode
 (add-to-list 'load-path "~/.emacs.d/impatient-mode")
 (require 'impatient-mode)
 
-;; Add hooks for skewer mode
 (add-hook 'js2-mode-hook 'skewer-mode)
 (add-hook 'web-mode-hook 'skewer-css-mode)
 (add-hook 'web-mode-hook 'skewer-html-mode)
 
-;; NOTE: Prelude also has good programming language defaults via modules.
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 8. Miscellaneous Stuff @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; Hippie expand is dabbrev expand on steroids
 (setq hippie-expand-try-functions-list '(try-expand-dabbrev
                                          try-expand-dabbrev-all-buffers
                                          try-expand-dabbrev-from-kill
@@ -558,11 +445,9 @@ this with to-do items than with projects or headings."
                                          try-complete-lisp-symbol-partially
                                          try-complete-lisp-symbol))
 
-;; Use hippie-expand instead of dabbrev
 (global-set-key (kbd "M-/") #'hippie-expand)
 (global-set-key (kbd "s-/") #'hippie-expand)
 
-;; Delighted: no clutter - C-h m instead for active minor modes
 (delight '((auto-complete-mode nil "auto-complete")
            (beacon-mode nil "beacon")
            (company-mode nil "company")
@@ -580,13 +465,11 @@ this with to-do items than with projects or headings."
            (yas-minor-mode nil "yasnippet")
            (emacs-lisp-mode "EL" :major)))
 
-;; BBDB: Allow for adding birthdays to records
 (defadvice bbdb-read-new-record (after wicked activate)
   "Prompt for the birthdate as well."
   (bbdb-record-putprop ad-return-value 'birthdate
                        (bbdb-read-string "Birthdate (YYYY.MM.DD): ")))
 
-;; Browsing choices: EWW or Firefox
 (defalias 'gk-urls-external-browser 'browse-url-xdg-open)
 (defun gk-browse-url (&rest args)
   "Prompt for whether or not to browse with EWW, if no browse
@@ -597,6 +480,13 @@ with external browser."
      'gk-urls-external-browser)
    args))
 (setq browse-url-browser-function #'gk-browse-url)
+
+(defun eww-new ()
+  (interactive)
+  (let ((url (read-from-minibuffer "Enter URL or keywords: ")))
+    (switch-to-buffer (generate-new-buffer "eww"))
+    (eww-mode)
+    (eww url)))
 
 ;; End init file...
 (provide 'rtg-init)
