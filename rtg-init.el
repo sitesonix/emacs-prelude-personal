311554ddb1c78822882e9bca21506a9678a6ec45
RTG <ryan@sitesonix.net>
2017-09-19 22:02:15 -0500

    Add some basic counsel and swiper integration

--- rtg-init.el
+++ rtg-init.el
@@ -50,6 +50,7 @@
                             bbdb
                             bookmark+
                             company-web
+                            counsel
                             ctags
                             dired+
                             emmet-mode
@@ -70,6 +71,7 @@
                             project-explorer
                             simple-httpd
                             skewer-mode
+                            swiper
                             twittering-mode
                             web-beautify
                             ztree))
@@ -266,9 +268,12 @@
               filename-and-process)))
 
 (ivy-mode 1)
-
 (setq ivy-use-virtual-buffers t)
-(setq ivy-count-format "(%d/%d) ")
+(setq enable-recursive-minibuffers t)
+(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
 
 (setq prelude-guru nil)
 
